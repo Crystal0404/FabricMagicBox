7f66ce3bfe4a43bd0a86dd15ede309d3e3fabed4
Crystal0404 <syh040325@163.com>
2023-11-21 18:30:59 +0800

    换用Inject取缔原Overwriye,提升兼容性

--- src/main/java/crystal0404/magicbox/mixin/ShulkerboxMixin.java
+++ src/main/java/crystal0404/magicbox/mixin/ShulkerboxMixin.java
@@ -7,16 +7,14 @@ import net.minecraft.screen.ScreenHandler;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ShulkerBoxBlock.class)
 public class ShulkerboxMixin {
-    /**
-     * @author
-     * @reason
-     */
-    @Overwrite
-    public int getComparatorOutput(BlockState state, World world, BlockPos pos) {
-        return ScreenHandler.calculateComparatorOutput((Inventory)((Object)world.getBlockEntity(pos)));
+    @Inject(at = @At("TAIL"), method = "getComparatorOutput", cancellable = true)
+    public void MagicBoxMixin(BlockState state, World world, BlockPos pos, CallbackInfoReturnable<Integer> cir){
+        cir.setReturnValue(ScreenHandler.calculateComparatorOutput((Inventory)((Object)world.getBlockEntity(pos))));
     }
 }
